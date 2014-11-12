03491267bec882e719d8a18266d54b758adec51c
Tobias Kloht <tobias.kloht@gmail.com>
2014-11-12 18:48:27 +0100

    implemented karatsuba algorithm. seems to work.

--- src/com/tkloht/Main.java
+++ src/com/tkloht/Main.java
@@ -15,9 +15,8 @@ public class Main {
         BigInteger x = readNumber();
         System.out.println("Good job! Now another one.");
         BigInteger y = readNumber();
-        BigInteger result = x.multiply(y);
+        BigInteger result = karatsuba(x, y);
         System.out.println("Result:\n" + result);
-	// write your code here
     }
 
     public static BigInteger readNumber(){
@@ -44,4 +43,37 @@ public class Main {
         }
         return result;
     }
+
+    public static int getPivot(BigInteger x, BigInteger y){
+        int length = Math.max(x.bitLength(), y.bitLength());
+        // number of bits / 2, rounded up
+        return (length /2) + (length % 2);
+    }
+
+    public static BigInteger karatsuba (BigInteger x, BigInteger y) {
+        if (x.compareTo(BigInteger.TEN) < 0 || y.compareTo(BigInteger.TEN) < 0 ) {
+            return BigInteger.valueOf(x.intValue() * y.intValue());
+        }
+        int pivot = getPivot(x, y);
+
+        // x = a + 2^pivot b
+        BigInteger b = x.shiftRight(pivot);
+        BigInteger a = x.subtract(b.shiftLeft(pivot));
+
+        // y = c + 2^pivot d
+        BigInteger d = y.shiftRight(pivot);
+        BigInteger c = y.subtract(d.shiftLeft(pivot));
+
+        // recursion
+        BigInteger ac = karatsuba(a, c);
+        BigInteger bd = karatsuba(b, d);
+        BigInteger abcd = karatsuba(a.add(b), c.add(d));
+
+        return ac.add(
+            abcd.subtract(ac).subtract(bd).shiftLeft(pivot).add(
+                    bd.shiftLeft(2*pivot)
+            )
+        );
+    }
+
 }
